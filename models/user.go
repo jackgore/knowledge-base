77cba045cecd6742038b07e1f916319a3aad3dac
Jonathon Gore <jackgore@hotmail.ca>
2018-02-18 23:09:36 -0500

    Added fields to user struct

--- models/user.go
+++ models/user.go
@@ -6,7 +6,10 @@ import (
 
 type User struct {
 	ID        int       `json:"id"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
+	Bio       string    `json:"bio"`
 	JoinedOn  time.Time `json:"joined-on"`
 }
