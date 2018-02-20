74d2a5fb8b2b178ff2730befbd3466fcc3196db0
Jonathon Gore <jackgore@hotmail.ca>
2018-02-19 21:55:45 -0500

    Updated run tests script

--- runTests.sh
+++ runTests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Install our project and output any errors
-go install
+echo -n 'Building project...'
+go install -v
+echo 'finished'
+
 
 # Run our server
 knowledge-base > test_logs.txt 2>&1 &
