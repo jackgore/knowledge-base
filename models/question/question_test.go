808d019f06622b33126fee70a78915c582717fcb
Jonathon Gore <jackgore@hotmail.ca>
2018-05-16 20:18:58 -0400

    Added more tests to question package

--- models/question/question_test.go
+++ models/question/question_test.go
@@ -16,6 +16,20 @@ func (s *QuestionTestSuite) SetupSuite() {
 	log.SetOutput(ioutil.Discard)
 }
 
+func (s *QuestionTestSuite) TestValidateQuestion() {
+	s.NotNil(Validate(Question{}))
+}
+
+func (s *QuestionTestSuite) TestValidateQuestionContent() {
+	s.NotNil(validateQuestionTitle(""))
+	s.NotNil(validateQuestionTitle("abc"))
+}
+
+func (s *QuestionTestSuite) TestValidateQuestionTitle() {
+	s.NotNil(validateQuestionTitle(""))
+	s.NotNil(validateQuestionTitle("abc"))
+}
+
 func (s *QuestionTestSuite) TestValidateID() {
 	s.NotNil(validateID(-1))
 	s.NotNil(validateID(-9))
