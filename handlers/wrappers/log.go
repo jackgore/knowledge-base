dbf2a3613deec8672a6c691ddca78c99f03a59ac
Jonathon Gore <jackgore@hotmail.ca>
2018-05-27 22:23:28 -0400

    Updated wrapper to log response code

--- handlers/wrappers/log.go
+++ handlers/wrappers/log.go
@@ -6,10 +6,28 @@ import (
 	"time"
 )
 
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
 func Log(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lrw := NewLoggingResponseWriter(w)
+
 		start := time.Now()
-		handler.ServeHTTP(w, r)
-		log.Printf("%v %v took %v seconds", r.Method, r.URL.Path, time.Since(start).Seconds())
+		handler.ServeHTTP(lrw, r)
+		statusCode := lrw.statusCode
+
+		log.Printf("%v %v %v took %v seconds", statusCode, r.Method, r.URL.Path, time.Since(start).Seconds())
 	})
 }
