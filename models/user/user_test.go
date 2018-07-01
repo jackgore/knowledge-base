8156d39e9222f2eff75870bfb6c50a841614c33e
Jonathon Gore <jackgore@hotmail.ca>
2018-07-01 12:39:39 -0400

    models/user: added tests for email validation

--- models/user/user_test.go
+++ models/user/user_test.go
@@ -4,29 +4,76 @@ import (
 	"io/ioutil"
 	"log"
 	"testing"
-
-	"github.com/stretchr/testify/suite"
 )
 
-type UserTestSuite struct {
-	suite.Suite
-}
-
-func (s *UserTestSuite) SetupSuite() {
+func init() {
 	log.SetOutput(ioutil.Discard)
 }
 
-func (s *UserTestSuite) TestValidate() {
-	s.NotNil(Validate(User{ID: -1}))
+var validateEmailTests = []struct {
+	email   string
+	success bool
+}{
+	{"", false},
+	{" ", false},
+	{"email", false},
+	{"@", false},
+	{"@com", false},
+	{"jack@", false},
+	{"jack@test.com", true},
 }
 
-func (s *UserTestSuite) TestValidateID() {
-	s.NotNil(validateID(-1))
-	s.NotNil(validateID(-9))
-	s.Nil(validateID(1))
-	s.Nil(validateID(100))
+var validateIDTests = []struct {
+	id      int
+	success bool
+}{
+	{-1, false},
+	{-9, false},
+	{1, true},
+	{100, true},
 }
 
-func TestSuite(t *testing.T) {
-	suite.Run(t, new(UserTestSuite))
+var validateTests = []struct {
+	user    User
+	success bool
+}{
+	{User{ID: -1}, false},
+}
+
+func TestValidateEmail(t *testing.T) {
+	for _, test := range validateEmailTests {
+		if (ValidateEmail(test.email) == nil) != test.success {
+			text := "succeed"
+			if !test.success {
+				text = "fail"
+			}
+			t.Errorf("Expected test to %v for email: %v but it did not", text, test.email)
+		}
+	}
+
+}
+
+func TestValidate(t *testing.T) {
+	for _, test := range validateTests {
+		if (Validate(test.user) == nil) != test.success {
+			text := "succeed"
+			if !test.success {
+				text = "fail"
+			}
+			t.Errorf("Expected test to %v for user: %+v but it did not", text, test.user)
+		}
+	}
+
+}
+
+func TestValidateID(t *testing.T) {
+	for _, test := range validateIDTests {
+		if (validateID(test.id) == nil) != test.success {
+			text := "succeed"
+			if !test.success {
+				text = "fail"
+			}
+			t.Errorf("Expected test to %v for id: %v but it did not", text, test.id)
+		}
+	}
 }
